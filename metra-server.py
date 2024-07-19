5306f531939e78443c5dde8806ee3b7313000fd0
Liam Fruzyna <liam@fruzyna.net>
2024-07-19 15:01:18 -0500

    Show additional upcoming stops on webapp

--- metra-server.py
+++ metra-server.py
@@ -44,13 +44,19 @@ async def index():
             </body>\
         </html>'
 
+def build_line(stop) -> str:
+    return f"#{stop.train} - {stop.time_until}<br>"
+
 # build stop page using data from API and query parameters
 @app.get('/stop', response_class=HTMLResponse)
-async def stop(line="UP-NW", stop="DESPLAINES"):
+async def stop(line="UP-NW", stop="DESPLAINES", count=3):
     line = line.upper()
     stop = stop.upper()
 
-    first_inbound, first_outbound = metra.get_next(line, stop)
+    inbound, outbound = metra.get_next(line, stop, count=int(count))
+    if int(count) == 1:
+        inbound = [inbound]
+        outbound = [outbound]
 
     return f'<!DOCTYPE html>\
         <html lang="en">\
@@ -64,10 +70,12 @@ async def stop(line="UP-NW", stop="DESPLAINES"):
                 <center>\
                     <h2>Next {line} Trains at </h2>\
                     <h1>{stop}</h1>\
-                    Inbound: #{first_inbound.train}<br>\
-                    <h2>{LIVE_STR if first_inbound.live else ""}{first_inbound.minutes} minutes</h2>\
-                    Outbound: #{first_outbound.train}<br>\
-                    <h2>{LIVE_STR if first_outbound.live else ""}{first_outbound.minutes} minutes</h2>\
+                    <h3>Inbound - #{inbound[0].train}<h3>\
+                    <h2>{LIVE_STR if inbound[0].live else ""}{inbound[0].time_until}</h2>\
+                    {''.join([build_line(inbound[i]) for i in range(1, len(inbound))])}\
+                    <h3>Outbound - #{outbound[0].train}</h3>\
+                    <h2>{LIVE_STR if outbound[0].live else ""}{outbound[0].time_until}</h2>\
+                    {''.join([build_line(outbound[i]) for i in range(1, len(outbound))])}\
                 </center>\
             </body>\
         </html>'
